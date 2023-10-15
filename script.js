83823cb481670052727662186b03a8c1208d8f56
MECK4 <147999358+MECK4@users.noreply.github.com>
2023-10-16 00:47:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,3 @@ function copyEmail()
     navigator.clipboard.writeText("officialdamagency@gmail.com");
     mail.title = "Email Address Copied Successfully"; 
 }
-
-function toggleDropDown()
-{
-    let menu = document.getElementById("dropDownOffers");
-    if(menu.style.display == "none")
-    {
-        menu.style.display = "block";
-    }
-    else {
-        menu.style.display = "none";
-    }
-}
